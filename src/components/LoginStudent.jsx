11fda6c720674f6232714095ce096efb9249262c
sourabhkanolkar <sourabhkanolkar17@gmail.com>
2025-03-07 13:17:31 +0530

    logindesign better before meeting poonam

--- src/components/LoginStudent.jsx
+++ src/components/LoginStudent.jsx
@@ -30,21 +30,21 @@ export default function LoginStudent() {
            
             <div className="col-12 mt-3 ">
             <div className="title">
-                <h2 className='text-center'>LOGIN STUDENT</h2>
+                <h2 className='text-center' style={{fontFamily:"sans-serif",fontWeight:"bold"}}>LOGIN STUDENT</h2>
             </div>
-            <form onSubmit={handleSubmit }>
+            <form onSubmit={handleSubmit } style={{width:"364px",height:"469px",display:"block",margin:"auto"}}>
   <div class="mb-3">
     <label for="exampleRollNo" class="form-label">RollNo</label>
-    <input type="text" class="form-control" onChange={(e)=>setRollno(e.target.value)} id="exampleRollNo" aria-describedby="emailHelp"/>
+    <input type="text" style={{width:"364px",height:"53px",borderRadius:"15px"}} class="form-control" onChange={(e)=>setRollno(e.target.value)} id="exampleRollNo" aria-describedby="emailHelp"/>
 
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" onChange={(e)=>setPassword(e.target.value)} id="exampleInputPassword1"/>
+    <input type="password" style={{width:"364px",height:"53px",borderRadius:"15px"}} class="form-control" onChange={(e)=>setPassword(e.target.value)} id="exampleInputPassword1"/>
   </div>
   
-  <button type="submit" class="btn btn-primary">Login</button>
-    <Link to="/LoginTeacher"> Are You A Teacher?</Link>
+  <button type="submit" style={{width:"124px",height:"52px",backgroundColor:"#5038ED",display:"block",margin:"auto"}} class="btn btn-primary">Login</button>
+    {/* <Link to="/LoginTeacher"> Are You A Teacher?</Link> */}
 </form>
             </div>
         </div>
